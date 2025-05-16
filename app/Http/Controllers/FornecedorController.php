1614079d9320f38b99289e9df18b14654a5f0ed6
Samuel <samuelbts1993l@gmail.com>
2025-05-16 17:18:37 -0300

    estudos dia 16/05/2025

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'S', 'cnpj' => '000'],
+            1 => ['nome' => 'Fornecedor 2', 'status' => 'S'],
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,16 +1,18 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentario descartado pelo interpretador do blade --}}
-
-{{ 'Testando!' }}
-<br>
-<?= 'Testando!' ?>
-<br>
-
 @php
-    // Para comentarios de uma linha
-    /* 
-     Para comentarios de multiplas linhas
-    */
-    echo 'Testando!'
+    
 @endphp
+
+@isset($fornecedores)    
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+    CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @empty($fornecedores[0]['cnpj'])
+        - Vazio
+    @endempty
+    @endisset
+@endisset
