56f7a3f5b1cc96ae42a0cee11f3e523c7d64c71e
Samuel <samuelbts1993l@gmail.com>
2025-05-16 16:25:53 -0300

    primeiro comite

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,16 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentario descartado pelo interpretador do blade --}}
+
+{{ 'Testando!' }}
+<br>
+<?= 'Testando!' ?>
+<br>
+
+@php
+    // Para comentarios de uma linha
+    /* 
+     Para comentarios de multiplas linhas
+    */
+    echo 'Testando!'
+@endphp
