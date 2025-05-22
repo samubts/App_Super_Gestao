2f9259363283b2a8db5a79844b8003b8003744c0
Samuel <samuelbts1993l@gmail.com>
2025-05-22 17:43:01 -0300

    Estudos do dia 22/05/2025

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+use Illuminate\Support\Facades\DB;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // criando por instancia do metodo
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com';
+        $fornecedor->save();
+
+        // criando atraves do metodo create (atenção para o atributo fillable na classe)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor200.com'
+        ]);
+
+        //por insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor300.com'
+        ]);
+    }
+}
